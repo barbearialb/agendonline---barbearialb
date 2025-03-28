a6ebf563167695d5853cb844ee732147d3343891
barbearialb <barbearialucasborges@gmail.com>
2025-03-28 02:54:51 -0300

    si.py

--- si.py
+++ si.py
@@ -298,22 +298,22 @@ with st.form("agendar_form"):
 
     horarios_disponiveis = horarios_base_agendamento[:]
 
+   
    if barbeiro_selecionado != "Sem preferência":
-        horarios_disponiveis = []
-        for horario in horarios_base_agendamento:
-            if verificar_disponibilidade(data_agendamento, horario, barbeiro_selecionado):
-                horarios_disponiveis.append(horario)
-    else:
-        horarios_disponiveis = horarios_base_agendamento[:]  # Começa com todos os horários
-        for horario in horarios_base_agendamento:
-            disponivel_para_todos = True
-            for barbeiro in barbeiros:
-                if not verificar_disponibilidade(data_agendamento, horario, barbeiro):
-                    disponivel_para_todos = False
-                    break
-            if not disponivel_para_todos:
-                horarios_disponiveis.remove(horario)
-
+    horarios_disponiveis = []
+    for horario in horarios_base_agendamento:
+        if verificar_disponibilidade(data_agendamento, horario, barbeiro_selecionado):
+            horarios_disponiveis.append(horario)
+else:
+    horarios_disponiveis = horarios_base_agendamento[:]  # Começa com todos os horários
+    for horario in horarios_base_agendamento:
+        disponivel_para_todos = True
+        for barbeiro in barbeiros:
+            if not verificar_disponibilidade(data_agendamento, horario, barbeiro):
+                disponivel_para_todos = False
+                break
+        if not disponivel_para_todos:
+            horarios_disponiveis.remove(horario)
     horario_agendamento = st.selectbox("Horário", horarios_disponiveis)
     servicos_selecionados = st.multiselect("Serviços", list(servicos.keys()))
 
