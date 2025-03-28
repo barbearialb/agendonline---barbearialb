b85ab51244018a6a11e6db3f29e189bf345013ca
barbearialb <barbearialucasborges@gmail.com>
2025-03-28 01:43:21 -0300

    Add files via upload

--- si.py
+++ si.py
@@ -223,7 +223,7 @@ if 'date_changed' not in st.session_state:
 def handle_date_change():
     verificar_disponibilidade.clear()
     st.session_state['date_changed'] = not st.session_state['date_changed']
-    st.experimental_rerun()
+    st.rerun()
 
 data_agendamento_obj = st.date_input("Data para visualizar disponibilidade", min_value=datetime.today(), key="data_input_widget", on_change=handle_date_change)
 data_para_tabela = st.session_state.get("data_input_widget", datetime.today()).strftime('%d/%m/%Y')
